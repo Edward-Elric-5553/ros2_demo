b16e686ec0df7708a05891a0729165f2b3959552
Edward-Elric-5553 <edward.elric.5553@gmail.com>
2025-08-05 18:48:02 +0800

    chapt2 pkg create python node.

---
+++ chapt2/install/demo_python_pkg/lib/python3.10/site-packages/demo_python_pkg/python_node.py
@@ -0,0 +1,14 @@
+import rclpy
+from rclpy.node import Node 
+
+def main():
+    rclpy.init()
+    
+    node = Node("python_node")
+    node.get_logger().info('hello python node')
+    node.get_logger().warn('hello python node')
+    node.get_logger().error('hello python node')
+
+    rclpy.spin(node)
+
+    rclpy.shutdown()
