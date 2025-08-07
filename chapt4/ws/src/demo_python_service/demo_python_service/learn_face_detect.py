78d472fb417c33adae1e0ba0edfb5c184118d681
Edward-Elric-5553 <edward.elric.5553@gmail.com>
2025-08-08 01:22:17 +0800

    chapt4 demo face detect Server/Client

---
+++ chapt4/ws/src/demo_python_service/demo_python_service/learn_face_detect.py
@@ -0,0 +1,19 @@
+import face_recognition
+import cv2
+from ament_index_python.packages import get_package_share_directory
+
+def main():
+    default_image_path = get_package_share_directory('demo_python_service') + '/resource/default.jpg'
+    print(f"path of default.jpg : {default_image_path}") 
+
+    image = cv2.imread(default_image_path)
+
+    face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=1, model='hog')
+
+    for top,right,bottom,left in face_locations :
+        cv2.rectangle(image,(left,top),(right,bottom),(255,0,0),4)
+
+    cv2.imshow('Face Detector result', image)
+
+    cv2.waitKey(0)
+
